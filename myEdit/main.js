13026886e593c1748b878ff6e23504b8b0d5ad5d
lu_hb <lu_hb@centit.com>
2020-03-16 16:04:43 +0800

    1、初步的文本编辑

--- myEdit/main.js
+++ myEdit/main.js
@@ -1,9 +1,45 @@
+function hasAbc(str) {
+    const testRegExp = /[a-z]+/
+    return testRegExp.test(str)
+}
+
+
 const app = new Vue({
     el: '#app',
     data() {
         return {
             type: 'text',
-            message: 'Hello World!'
+            message: 'Hello World!',
+            editWindowStyle: {}
         }
-    }
+    },
+    mounted() {
+        this.onEventListener()
+        this.editWindowStyle = this.$refs.editorWindow.style
+    },
+    methods: {
+        changeFontSize(e) {
+            const value = e.target.value
+           if(!hasAbc(value)) {
+            this.editWindowStyle.fontSize = value + 'px'
+           } else {
+            this.editWindowStyle.fontSize = value
+           }
+           this.editWindowStyle.textIndent = 2 * parseInt(this.editWindowStyle.fontSize)
+        },
+        firstLineIndent () {
+            if(this.editWindowStyle.fontSize === '') {
+                this.editWindowStyle.textIndent = '24px'
+            } else {
+                this.editWindowStyle.textInden = 2 * parseInt(this.$refs.editorWindow.style.fontSize) + 'px'
+            }
+        },
+        simpleCommand(command) {
+            document.execCommand(command)
+        },
+        afterEnter (){
+        },
+        onEventListener() {
+        }
+    },
 })
