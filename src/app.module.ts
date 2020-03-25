0904799038bffdd4bb537e17663802c69ad2b0df
lu_hb <lu_hb@centit.com>
2020-03-25 17:03:07 +0800

    添加user模块，修改blog module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module'
+import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
 import { BlogController } from './blog/blog.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, UsersModule, TypeOrmModule.forRoot()],
   controllers: [AppController, BlogController],
   providers: [AppService],
 })
