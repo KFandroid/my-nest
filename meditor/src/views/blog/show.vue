2b772e079ac2db2bf5fd3dc5e8d7046fd68f5068
lu_hb <lu_hb@centit.com>
2020-03-26 17:32:49 +0800

    show blog

--- meditor/src/views/blog/show.vue
+++ meditor/src/views/blog/show.vue
@@ -22,8 +22,17 @@ export default {
   },
   async mounted() {
       const res = await axios.get(`/blog/${this.$route.params.blogId}`)
+      
       console.log('res is', res)
+      res.data.text.replace('contenteditable="true"', '')
+      console.log(res.data.text)
       this.blog = res.data
+      this.$nextTick(() => {
+         const editorWind = document.getElementsByClassName("editor")[0]
+         editorWind.contenteditable = false
+         console.log(editorWind)
+      })
+      
   }
 }
 </script>
