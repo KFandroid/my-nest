0a59651cdd12884bd5391f8d47bd46aad1108808
lu_hb <lu_hb@centit.com>
2020-03-30 17:18:10 +0800

    vscode上调试nest 添加md5验证文件是否已上传

--- meditor/vue.config.js
+++ meditor/vue.config.js
@@ -1,5 +1,12 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
     devServer: {
        proxy: 'http://localhost:3000'
+    },
+    configureWebpack: {
+        plugins: [
+            new BundleAnalyzerPlugin()
+          ]
     }
 }
